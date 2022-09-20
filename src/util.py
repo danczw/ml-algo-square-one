4c280e66754821eb6ac3498cf16ec7605c562050
Daniel <dev.dc@pm.me>
2022-09-20 15:25:03 +0200

    add color printing

--- src/util.py
+++ src/util.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+# print colors
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # cumpute accuracy of classification algorithm
 def accuracy(y_true:np.ndarray, y_pred:np.ndarray) -> float:
     return np.sum(y_true == y_pred) / len(y_true)
